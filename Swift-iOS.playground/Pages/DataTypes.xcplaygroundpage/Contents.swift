e172762244e09b266e65d84d024da5a82745b75c
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-21 10:34:12 +0100

    String Handling, Data Types added

---
+++ Swift-iOS.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+/*
+ 'Data Types' in Swift
+ */
+
+// Integer
+var a: Int = 40
+print("This is an Integer value: \(a)")
+
+// String
+var b: String = "Nadhu"
+print("This is a String value: \(b)")
+
+// Floating Point
+var c: Float = 40.123
+print("This is a Floating Point value: \(c)")
+
+// Double
+var d: Double = 53.2345646363465
+print("This is a Double value: \(d)")
+
+// Boolean
+var e: Bool = true
+print("This is a Boolean value: \(e)")
---
+++ Swift-iOS.playground/Pages/StringHandling.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/*
+ String Handling - 'Interpolation' and 'Concatenation' in Swift
+ */
+
+// 'String Interpolation' in Swift
+print("The result of 5+3 is: \(5+3)")
+
+// 'String Concatenation' in Swift
+let password = "God is " + "Great"
+print("My Password is: \(password)")
